a1ef506572b3e9e8445d00dc940a0c41843fe415
Kavin Gnanapandithan <kavin.balag@gmail.com>
2023-11-22 15:38:17 -0500

    Able to read accelerometer data in full

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@
 #![allow(unused_imports)]
 
 
-use cortex_m_semihosting::{hprint, hprintln};
+//use cortex_m_semihosting::{hprint, hprintln};
 // pick a panicking behavior
 use panic_halt as _; // you can put a breakpoint on `rust_begin_unwind` to catch panics
 // use panic_abort as _; // requires nightly
@@ -15,10 +15,11 @@ use cortex_m_rt::entry;
 use generic_array::{ArrayLength, GenericArray};
 use core::cell::{Cell, RefCell};
 use stm32f4xx_hal::{
-    i2c::Mode,
-    pac::{self},
-    prelude::*,
+    i2c::{self},
+    pac::{self, I2C1},
+    prelude::*, gpio::alt::i2c1,
 };
+use rtt_target::{rprintln, rprint, rtt_init_print};
 
 // Slave address 
 const SLAVE_ADDRESS: u8 = 0x6A; // LSB is 1 if SDO/SA0 is connect to usplly voltage, 0 otherwise
@@ -37,16 +38,16 @@ const STATUS_REG: u8 = 0b0001_1110;
 // Ex: OUTX_H_G + OUTX_L_G
 
 // Angular rate pitch axis (X) output
-const OUTX_L_G: u8 = 0b0010_0010;
-const OUTX_H_G: u8 = 0b0010_0011;
+const OUTX_L_G: u8 = 0x22;
+const OUTX_H_G: u8 = 0x23;
 
 // Angular rate roll axis (Y) output
-const OUTY_L_G: u8 = 0b0010_0100;
-const OUTY_H_G: u8 = 0b0010_0101;
+const OUTY_L_G: u8 = 0x24;
+const OUTY_H_G: u8 = 0x25;
 
 // Angular rate yaw axis (Z) output
-const OUTZ_L_G: u8 = 0b0010_0110;
-const OUTZ_H_G: u8 = 0b0010_0111;
+const OUTZ_L_G: u8 = 0x26;
+const OUTZ_H_G: u8 = 0x27;
 
 // ACCELEROMETER CONFIG REG
 const CTRL1_XL: u8 = 0x10;
@@ -55,22 +56,22 @@ const CTRL1_XL: u8 = 0x10;
 // ACCELEROMETER RATE OUT REGISTERS
 
 // Accel rate X axis (X) output
-const OUTX_L_A: u8 = 0b0010_1000;
-const OUTX_H_A: u8 = 0b0010_1001;
+const OUTX_L_A: u8 = 0x28;
+const OUTX_H_A: u8 = 0x29;
 
 // Accel rate Y axis (Y) output
-const OUTY_L_A: u8 = 0b0010_1010;
-const OUTY_H_A: u8 = 0b0010_1011;
+const OUTY_L_A: u8 = 0x2A;
+const OUTY_H_A: u8 = 0x2B;
 
 // Accel rate Z axis (Z) output
-const OUTZ_L_A: u8 = 0b0010_1100;
-const OUTZ_H_A: u8 = 0b0010_1101;
+const OUTZ_L_A: u8 = 0x2C;
+const OUTZ_H_A: u8 = 0x2D;
 
 
 #[entry]
 fn main() -> ! {
-    // asm::nop(); // To not have main optimize to abort in release mode, remove when you add code
-    
+    rtt_init_print!();
+
     let dp = pac::Peripherals::take().unwrap();
 
     let rcc = dp.RCC.constrain();
@@ -88,11 +89,12 @@ fn main() -> ! {
     );
 
     let mut buffer: [u8; 2] = [0; 2];
+    let mut word: i16;
 
 
     match i2c.write_read(SLAVE_ADDRESS, &[WHO_AM_I], &mut buffer) {
-        Ok(_) => hprintln!("The chip's id is: {:#b}", buffer[0]).unwrap(),
-        Err(_) => hprintln!("Failed to read").unwrap(),
+        Ok(_) => rprintln!("The chip's id is: {:#b}", buffer[0]),
+        Err(_) => rprintln!("Failed to read"),
     }
 
     /* 
@@ -106,14 +108,48 @@ fn main() -> ! {
     // Configures Accelerometer
     // ODR_XL[7:4] = 0100; sets accelerometer to work at 104 Hz
     // FS[3:2] = 10; sets accelerometer full-scale selection to 4g
-    // LPF2_XL_EN = output from first stage digital filtering
-    // 0100 11 0 0
-    i2c.write(CTRL1_XL, &[0x4C]).unwrap(); 
+    // LPF2_XL_EN[1] = 1; output from first stage digital filtering
+    // 0100 10 1 0
+    i2c.write(SLAVE_ADDRESS, &[CTRL1_XL, 0x4A]).unwrap(); 
 
-    //let mut accel_buffer = [0u8; 1]; 
+    let mut accel_data:[f32; 3] = [0.0, 0.0, 0.0];
 
     loop {
+    
+        i2c.write_read(SLAVE_ADDRESS, &[OUTX_H_A], &mut buffer);
+        word = (buffer[0]  as i16) << 8;
+    
+        i2c.write_read(SLAVE_ADDRESS, &[OUTX_L_A], &mut buffer);
+        word |= buffer[0] as i16;
 
+        accel_data[0] = (word as f32) * 4.0/ 32768.0;
+
+        i2c.write_read(SLAVE_ADDRESS, &[OUTY_H_A], &mut buffer);
+        word = (buffer[0]  as i16) << 8;
+    
+        i2c.write_read(SLAVE_ADDRESS, &[OUTY_L_A], &mut buffer);
+        word |= buffer[0] as i16;
+
+        accel_data[1] = (word as f32) * 4.0/ 32768.0;
+
+        i2c.write_read(SLAVE_ADDRESS, &[OUTZ_H_A], &mut buffer);
+        word = (buffer[0]  as i16) << 8;
+    
+        i2c.write_read(SLAVE_ADDRESS, &[OUTZ_L_A], &mut buffer);
+        word |= buffer[0] as i16;
+
+        accel_data[2] = (word as f32) * 4.0/ 32768.0;
+
+        // rprint!("X: {}", accel_data[0]);
+        // rprint!(", Y: {}", accel_data[1]);
+        // rprintln!(", Z: {}", accel_data[2]);
+        
 
     }
+
+ 
+
+
 }
+
+
