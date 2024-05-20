559fbac506b1db03f8f569a2566c1cabc878b378
Kavin Gnanapandithan <kavin.balag@gmail.com>
2024-05-19 21:25:32 -0400

    Modified read_accel as well as removed phantomdata

--- src/lib.rs
+++ src/lib.rs
@@ -3,9 +3,7 @@
 
 extern crate embedded_hal as hal;
 
-use core::marker::PhantomData;
-
-use hal::blocking::i2c;
+use hal::blocking::i2c::{Write, WriteRead};
 
 // Slave address 
 const SLAVE_ADDRESS: u8 = 0x6A; // LSB is 1 if SDO/SA0 is connect to usplly voltage, 0 otherwise
@@ -59,104 +57,65 @@ const OUTZ_H_A: u8 = 0x2D;
 
 // lsm6dsox driver
 pub struct Lsm6dsox<I2C> {
-    i2c: PhantomData<I2C>,
+    i2c: I2C,
 }
 
 impl<I2C, E> Lsm6dsox<I2C> 
     where 
-        I2C: i2c::WriteRead<Error = E> + i2c::Write<Error = E>,
-{
-    pub fn new(_i2c: &I2C) -> Result<Self, E> {
-        let lsm6dsox = Lsm6dsox {
-            i2c: PhantomData,
-        };
-
+        I2C: WriteRead<Error = E> + Write<Error = E>,
+    {       
+    pub fn new(i2c: I2C) -> Result<Self, E> {
+        let mut lsm6dsox = Lsm6dsox { i2c };
         Ok(lsm6dsox)
     }
 
-    pub fn read_id(&self, i2c: &mut I2C) -> Result<u8, E> {
+    pub fn read_id(&mut self) -> Result<u8, E> {
 
         let mut buffer = [0u8, 1];
-        match i2c.write_read(SLAVE_ADDRESS, &[WHO_AM_I], &mut buffer) {
-            Ok(_) => Ok(buffer[0]),
-            Err(e) => Err(e),
-        }
+        &mut self.i2c.write_read(SLAVE_ADDRESS, &[WHO_AM_I], &mut buffer)?;
+
+        Ok(buffer[0])
     }
 
     // Configures Accelerometer
-    pub fn configure_accel(&self, i2c: &mut I2C) -> Result<(), E>{
+    pub fn configure_accel(&mut self) -> Result<(), E>{
         // ODR_XL[7:4] = 0100; sets accelerometer to work at 104 Hz
         // FS[3:2] = 10; sets accelerometer full-scale selection to 4g
         // LPF2_XL_EN[1] = 1; output from first stage digital filtering
         // 0100 10 1 0
         let configuration: u8 = 0x6A;
-        match i2c.write(SLAVE_ADDRESS, &[CTRL1_XL, configuration]) {
-            Ok(_) => Ok(()),
-            Err(e) => Err(e),
-        }
+        &mut self.i2c.write(SLAVE_ADDRESS, &[CTRL1_XL, configuration])?;
+        Ok(())
     }
 
     // Configures Gyroscope
-    pub fn configure_gyro(&self, i2c: &mut I2C) -> Result<(), E> {
+    pub fn configure_gyro(&mut self) -> Result<(), E> {
         // ODR_G[3:0] = 0110; sets gyroscope to work at 416 Hz
         // FS[1:0] = 11; sets full-scale selcetion to 2000dps
         // FS_125 = 0;
         let configuration: u8 = 0x6C;
-        match i2c.write(SLAVE_ADDRESS, &[CTRL2_G, configuration]) {
-            Ok(_) => Ok(()),
-            Err(e) => Err(e),
-        }
+        &mut self.i2c.write(SLAVE_ADDRESS, &[CTRL2_G, configuration])?;
+        Ok(())
     }
 
     // Read Accelerometer Data
-pub fn read_accel(&self, i2c: &mut I2C) -> Result<[f32; 3], E> {
-    let mut accel_data: [f32; 3] = [0.0, 0.0, 0.0];
-    let mut buffer: [u8; 2] = [0; 2];
-    let mut word: i16;
+    pub fn read_accel(&mut self) -> Result<[f32; 3], E> {
+        let mut accel_data: [f32; 3] = [0.0, 0.0, 0.0];
+        //let mut buffer: [u8; 2] = [0; 2];
+        let mut buffer: [u8; 6] = [0; 6];
 
-    if let Err(e) = i2c.write_read(SLAVE_ADDRESS, &[OUTX_H_A], &mut buffer) {
-        //rprintln!("Error reading OUTX_H_A: {:?}", e);
-        return Err(e);
+        const MULTI: u8 = 1 << 7;        
+        &mut self.i2c.write_read(SLAVE_ADDRESS, &[OUTX_L_A | MULTI], &mut buffer)?;
+        accel_data[0] = (((buffer[1] as i16) << 8 | buffer[0] as i16) as f32) * 4.0 / 32768.0;
+        accel_data[1] = (((buffer[3] as i16) << 8 | buffer[2] as i16) as f32) * 4.0 / 32768.0;
+        accel_data[2] = (((buffer[5] as i16) << 8 | buffer[4] as i16) as f32) * 4.0 / 32768.0;
+
+        Ok(accel_data)
     }
-    word = (buffer[0] as i16) << 8;
-
-    if let Err(e) = i2c.write_read(SLAVE_ADDRESS, &[OUTX_L_A], &mut buffer) {
-        return Err(e);
-    }
-    word |= buffer[0] as i16;
-
-    accel_data[0] = (word as f32) * 4.0 / 32768.0;
-
-    if let Err(e) = i2c.write_read(SLAVE_ADDRESS, &[OUTY_H_A], &mut buffer) {
-        return Err(e);
-    }
-    word = (buffer[0] as i16) << 8;
-
-    if let Err(e) = i2c.write_read(SLAVE_ADDRESS, &[OUTY_L_A], &mut buffer) {
-        return Err(e);
-    }
-    word |= buffer[0] as i16;
-
-    accel_data[1] = (word as f32) * 4.0 / 32768.0;
-
-    if let Err(e) = i2c.write_read(SLAVE_ADDRESS, &[OUTZ_H_A], &mut buffer) {
-        return Err(e);
-    }
-    word = (buffer[0] as i16) << 8;
-
-    if let Err(e) = i2c.write_read(SLAVE_ADDRESS, &[OUTZ_L_A], &mut buffer) {
-        return Err(e);
-    }
-    word |= buffer[0] as i16;
-
-    accel_data[2] = (word as f32) * 4.0 / 32768.0;
-
-    Ok(accel_data)
-}
 
 
     // Read Gyroscope Data
-    pub fn read_gyro(&self, i2c: &mut I2C) -> Result<[f32; 3], E> {
+    pub fn read_gyro(&mut self) -> Result<[f32; 3], E> {
 
         let mut gyro_data:[f32; 3] = [0.0, 0.0, 0.0];
         let mut buffer: [u8; 2] = [0; 2];
@@ -167,26 +126,26 @@ pub fn read_accel(&self, i2c: &mut I2C) -> Result<[f32; 3], E> {
         Z Gyro Calib: 2.0090027
         */
 
-        i2c.write_read(SLAVE_ADDRESS, &[OUTX_H_G], &mut buffer)?;
+        self.i2c.write_read(SLAVE_ADDRESS, &[OUTX_H_G], &mut buffer)?;
         word = (buffer[0]  as i16) << 8;
     
-        i2c.write_read(SLAVE_ADDRESS, &[OUTX_L_G], &mut buffer)?;
+        self.i2c.write_read(SLAVE_ADDRESS, &[OUTX_L_G], &mut buffer)?;
         word |= buffer[0] as i16;
 
         gyro_data[0] = (word as f32) * 2000.0/ 32768.0;
 
-        i2c.write_read(SLAVE_ADDRESS, &[OUTY_H_G], &mut buffer)?;
+        self.i2c.write_read(SLAVE_ADDRESS, &[OUTY_H_G], &mut buffer)?;
         word = (buffer[0]  as i16) << 8;
     
-        i2c.write_read(SLAVE_ADDRESS, &[OUTY_L_G], &mut buffer)?;
+        self.i2c.write_read(SLAVE_ADDRESS, &[OUTY_L_G], &mut buffer)?;
         word |= buffer[0] as i16;
 
         gyro_data[1] = (word as f32) * 2000.0/ 32768.0;
 
-        i2c.write_read(SLAVE_ADDRESS, &[OUTZ_H_G], &mut buffer)?;
+        self.i2c.write_read(SLAVE_ADDRESS, &[OUTZ_H_G], &mut buffer)?;
         word = (buffer[0]  as i16) << 8;
     
-        i2c.write_read(SLAVE_ADDRESS, &[OUTZ_L_G], &mut buffer)?;
+        self.i2c.write_read(SLAVE_ADDRESS, &[OUTZ_L_G], &mut buffer)?;
         word |= buffer[0] as i16;
 
         gyro_data[2] = (word as f32) * 2000.0/ 32768.0;
